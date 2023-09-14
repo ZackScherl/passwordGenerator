f5b0c682ac63dcb78298ef4b61873e3aa39d95c1
Zaxarybinx <zackscherl@gmail.com>
2023-09-13 21:45:55 -0400

    Updated ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # passwordGenerator
+
+The starter code was sourced from 
+The Coding Bootcamp repo: https://github.com/coding-boot-camp/friendly-parakeet
+
+I followed advice from Paul Keldson's youtube guide for advice on pseudocoding and using decomposition on the tasks at hand
+
+Michell Brito provided tutoring advice as I became stuck on getting my password to populate - I had a mistake on lines 39-42, she helped me
+understand the use of .push(...)
--- script.js
+++ script.js
@@ -2,19 +2,15 @@
 
 var generateBtn = document.querySelector("#generate");
 
-//var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 // broke characters into categories in order to enable individual selection upon confirm prompt dialogue boxes
 var numbersPass = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lowercasePass = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercasePass = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var symbolsPass = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 var allChars = [];
-//var password = "";
-// var minLength = 8;
-//var maxLength = 128;
 
-// 1. prompt user, [click the button to generate a password THEN I am presented with a series of prompts]
 
+// user clicks the button to generate a password and is presented with a series of prompts
 
 function generatePassword() {
 
@@ -23,7 +19,7 @@ function generatePassword() {
   // Paul Keldson suggestion adding this log to button as a way to visibly confirm our code is working
   console.log("Hey! You clicked the button!");
 
-  // when prompted for the length of the password, the choice is meant to 
+  // when prompted for the length of the password, the user choice is given a parameter this code supplies the range
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Your password does not meet the critia. Password must be between 8 and 128 characters in length.");
     return;
@@ -40,17 +36,13 @@ function generatePassword() {
     // Validate input with if statements to determine which characters get appended into the allChars variable based on user's answers
   }
   
-
-  //1: [abc]
-  //2: [def]
-  //3: [abc].push(...[def]) // [abcdef]
-
-  //4. [...[abc], ...[def]]
   if (passwordLowercase) { allChars.push(...lowercasePass)}
   if (passwordUppercase) { allChars.push(...uppercasePass)}
   if (passwordNumber) { allChars.push(...numbersPass)}
   if (passwordSymbol) { allChars.push(...symbolsPass)}
 
+
+  //loop is created to repeat the function until the length determined by user is met
   for (var i = 0; i < passwordLength; i++) {
     var randomPass = Math.floor(Math.random() * Math.floor(allChars.length));
     password += allChars[randomPass];
@@ -59,7 +51,7 @@ function generatePassword() {
   return password
 }
 
-// 3. generate password 
+// generate password
 
 function writePassword() {
   var password = generatePassword();
